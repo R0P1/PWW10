ce7b365761a804148c1c31b1d31def14df895c70
FII14 <121723975+FII14@users.noreply.github.com>
2023-08-20 16:00:36 +0700

    Update and rename pww10.py to main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Dibuat oleh: Rofi [FII14]
+
 import subprocess
 import os
 import re
@@ -22,29 +24,20 @@ if tabulate_available:
             profil_wifi = {}  # Buat kamus kosong untuk menyimpan informasi profil Wi-Fi
             profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output=True).stdout.decode()
 
-            # Periksa apakah profil Wi-Fi memiliki kunci keamanan atau tidak
             if re.search("Kunci Keamanan           : Tidak Ada", profile_info):
                 continue  # Lanjut ke profil Wi-Fi berikutnya jika tidak memiliki kunci keamanan
             else:
-                # Simpan nama SSID dari profil Wi-Fi dalam kamus profil_wifi
                 profil_wifi["ssid"] = name
-
-                # Dapatkan informasi lengkap profil Wi-Fi, termasuk kata sandi
                 profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output=True).stdout.decode()
-
-                # Cari kata sandi menggunakan ekspresi reguler
                 password = re.search("Konten Kunci            : (.*)\r", profile_info_pass)
 
-                # Simpan kata sandi dalam kamus profil_wifi, atau None jika tidak ada kata sandi yang ditemukan
                 if password is None:
                     profil_wifi["password"] = None
                 else:
                     profil_wifi["password"] = password[1]
 
-                # Tambahkan kamus profil_wifi ke daftar_wifi
                 daftar_wifi.append(profil_wifi)
 
-    # Format informasi profil Wi-Fi yang diambil menjadi tabel
     header_tabel = ["SSID", "Kata Sandi"]
     data_tabel = []
     for profil_wifi in daftar_wifi:
@@ -52,6 +45,4 @@ if tabulate_available:
         password = profil_wifi["password"]
         data_tabel.append([ssid, password])
 
-    # Cetak tabel
     print(tabulate(data_tabel, headers=header_tabel, tablefmt="grid"))
-    
